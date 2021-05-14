afc00b287c95141518a6ef86a2323a5b9e151c7a
piotrgrzaslewicz <pg111@poczta.onet.pl>
2021-05-14 15:07:39 +0200

    initial commit

---
+++ src/main/java/pl/mrook/app/AppConfig.java
@@ -0,0 +1,13 @@
+package pl.mrook.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "pl.mrook")
+public class AppConfig implements WebMvcConfigurer {
+}
